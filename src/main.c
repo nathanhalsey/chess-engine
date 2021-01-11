0ed40c3de66201d9aadff0aabbfa6daa009e8f82
nathanhalsey <nhalsey@uwo.ca>
2021-01-10 22:56:08 -0500

    Started working on testing out my init function, but realized this shouldn't be here in main. Will update with code in debug_board.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include"headers.h"
 int main(int argc, char const *argv[])
 {
-    print_board(pawn_attacks(A6,white));
+    init_attack_vectors();
+    for (int i = 0; i < 64; i++){
+        //print_board(pawn_)
+    }
+    //print_board(pawn_attacks(H4,black));
     return 0;
 }
